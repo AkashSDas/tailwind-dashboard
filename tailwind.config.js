59a923098862a40ea52722246878f7c6d17bce55
AkashSDas <aakashdas368@gmail.com>
2021-10-20 20:32:39 +0530

    enable dark mode and set mode as jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  enabled: true,
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
